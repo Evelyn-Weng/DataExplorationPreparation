f5fa0aadc90e873ec5de2e73eae2bcbe89c57d58
XIAOHUI-WENG2020 <2020387@student.cct.ie>
2023-12-01 15:10:42 +0000

    Data Cleaning
    drop 'Beat' column, and using the mode value to replace the sector column's NA, got the final dataset Newcrime2018 dataset

--- CA1.R
+++ CA1.R
@@ -17,3 +17,12 @@ names(db)
 
 ## Before plot anything, check if there is any missing value
 any(is.na(db))
+## subdataset
+db$Occurred.Date <- as.Date(db$Occurred.Date, format = "%m/%d/%Y")
+
+# Selecting 2018 year's dataset
+data_2018 <- db[format(db$Occurred.Date, "%Y") == "2018", ]
+crime2018 <- data_2018
+# view the dataset
+View(crime2018)
+
